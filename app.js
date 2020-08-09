4912a6d6a092b572fc6aed7c3f5ad8f31457909f
harshitSharma <47037260+harshit8816@users.noreply.github.com>
2020-08-09 10:32:38 +0530

    Add files via upload
    the questions are same for all categories
    Will be updated supersoon

---
+++ app.js
@@ -0,0 +1,180 @@
+//Module Dependences
+
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const nodemailer = require('nodemailer');
+const mongoose = require('mongoose');
+const { time } = require('console');
+const { getMaxListeners } = require('process');
+
+
+//List of Variables
+const app = express();
+const hostname = '127.0.0.1';
+const port = process.env.PORT || '80';
+
+app.use(express.urlencoded({ extended: false }));
+
+
+const MONGODB_URI = 'mongodb+srv://Harshit:Harshit@1209@database-harshit.tdfrn.gcp.mongodb.net/database-harshit?retryWrites=true&w=majority'
+
+mongoose.connect(MONGODB_URI || 'mongodb://localhost/database-harshit', {
+useNewUrlParser: true,
+useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on('connected', () => {
+    console.log('Mongoose is connected!');
+});
+
+
+//body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+
+app.post("/", (req, res) => {
+    //Schema
+    const Schema = mongoose.Schema;
+    const newsletterSchema = new Schema({
+        email: String
+    });
+
+    //Model
+    const newsletter = mongoose.model('newsletter', newsletterSchema);
+    var newsletter_email = new newsletter(req.body);
+
+    //Newsletter
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'harshitsharma9270@gmail.com',
+            pass: 'Harshit@1209'
+        }
+    });
+
+    var mailOptions = {
+        from: 'harshitsharma9270@gmail.com',
+        to: newsletter_email,
+        subject: 'Sending Email using Node.js',
+        text: 'That was easy! #newsletter'
+    }
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+    res.send('Your email has been registered for newsletter!');
+});
+
+app.post("/contact",(req,res)=>{
+      //Schema
+    //   const Schema = mongoose.Schema;
+      const contactSchema = new Schema({
+          name:String,
+          email: String,
+          phone:String,
+          message:String
+      });
+  
+      //Model
+      const contact = mongoose.model('contact', contactSchema);
+      var newContact = new contact(req.body);
+
+      newContact.save().then((data) => {
+        res.send('Hey, Your message is sent to QuizTown!');
+    })
+        .catch((error) => {
+            res.status(400).send('Oops,Something wrong happened');
+        });
+});
+
+const home = fs.readFileSync('./views/index.html');
+const about = fs.readFileSync('./views/about.html');
+const categories = fs.readFileSync('./views/categories.html');
+const quizzes = fs.readFileSync('./views/quiz.html');
+const quizlevels = fs.readFileSync('./views/quizlevels.html');
+// const register = fs.readFileSync('./views/register.html'); 
+const contact = fs.readFileSync('./views/contact.html');
+// const signin = fs.readFileSync('./views/signin.html');
+// const prompt = fs.readFileSync('./views/prompt.html');
+const instruction = fs.readFileSync('./views/instruction.html');
+const style = fs.readFileSync('./views/index.css');
+const phone = fs.readFileSync('./views/phone.css');
+const data = fs.readFileSync('./views/data.js');
+const quizScript = fs.readFileSync('./views/quizScript.js');
+
+
+// For Serving Static Files
+app.use('/public', express.static('public'));
+
+//End points
+
+app.get('/', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(home);
+});
+app.get('/about', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(about);
+});
+app.get('/instruction', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(instruction);
+});
+// app.get('/prompt', (req, res) => {
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.end(prompt);
+// });
+app.get('/categories', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(categories);
+});
+app.get('/quiz', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(quizzes);
+});
+app.get('/quizlevels', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(quizlevels);
+});
+app.get('/contact', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(contact);
+});
+// app.get('/register', (req, res) => {
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.end(register);
+// });
+// app.get('/signin', (req, res) => {
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.end(signin);
+// });
+app.get('/index.css', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/css' });
+    res.end(style);
+});
+app.get('/phone.css', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/css' });
+    res.end(phone);
+});
+app.get('/data.js', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/javascript' });
+    res.end(data);
+});
+app.get('/quizScript.js', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/javascript' });
+    res.end(quizScript);
+});
+
+
+app.listen(port, () => {
+    console.log(`Server running at  http://${hostname}:${port}/`);
+});
---
+++ views/quizScript.js
@@ -0,0 +1,156 @@
+
+var cateId = localStorage.getItem("categoryValue");
+// console.log(cateId);
+var levelId = localStorage.getItem("levelValue");
+// console.log(levelId);
+
+
+var myCategoryimages = { "1": "public/categories/artAndLiterature.jpg", "2": "public/categories/generalKnowledge.jpg", "3": "public/categories/geography.jpg", "4": "public/categories/history.jpg", "5": "public/categories/music.jpg", "6": "public/categories/scienceAndNature.jpg", "7": "public/categories/sport.jpg", "8": "public/categories/tieBreak.jpg", "9": "public/categories/tvAndFilms,jpg" };
+
+var lengthofobject = data[cateId][levelId].length;
+// console.log(lengthofobject);
+
+var curPage = 0, correct = 0;
+var myAnswers = [];
+
+
+var newimage = document.getElementById("categoryWiseImage");
+var myHeader = document.getElementById("quizHeader");
+var myQuestion = document.getElementById("questions");
+var myAnswerz = document.getElementById("answer");
+var classname = document.getElementsByClassName("answer-btn");
+var progressBar = document.getElementById("progress-bar");
+var btnNext = document.getElementById("btnNext");
+var btnPrevious = document.getElementById("btnPrevious");
+var submitBtn = document.getElementById('Submit');
+
+checkPage();
+// console.log('hello');
+
+btnNext.addEventListener("click", moveNext);
+btnPrevious.addEventListener("click", moveBack);
+submitBtn.addEventListener("click", endQuiz);
+
+function myAnswer() {
+    var idAnswer = this.getAttribute("data-id");
+    //check for correct answer
+    myAnswers[curPage] = idAnswer;
+    if (data[cateId][levelId][curPage].correct == idAnswer) {
+        // console.log('Correct Answer');
+    } else {
+        // console.log('Wrong Answer');
+    }
+    addBox();
+}
+
+function addBox() {
+    for (var i = 0; i < myAnswerz.children.length; i++) {
+        var curNode = myAnswerz.children[i];
+        if (myAnswers[curPage] == (i + 1)) {
+            curNode.classList.add("selectAnswer");
+        } else {
+            curNode.classList.remove("selectAnswer");
+        }
+    }
+}
+
+function moveNext() {
+
+    //check if an answer has been made
+    if (myAnswers[curPage]) {
+        // console.log('Okay to proceed');
+        if (curPage < (lengthofobject - 1)) {
+            curPage++;
+            checkPage(curPage);
+        } else {
+            //check if quiz is complete
+            // console.log(curPage + ' ' + myQuiz.length);
+            if (lengthofobject >= curPage) {
+                endQuiz();
+            } else {
+                // console.log('end of Quiz Page' + curPage);
+            }
+        }
+    } else {
+        // console.log('not answered');
+    }
+}
+
+
+function endQuiz() {
+    if (myAnswers[(lengthofobject - 1)]) {
+        var output = "<div class='output'><h1>Quiz Results</h1><BR>";
+        var questionResult = "NA";
+        //console.log('Quiz Over');
+        for (var i = 0; i < myAnswers.length; i++) {
+            if (data[cateId][levelId][i].correct == myAnswers[i]) {
+                questionResult = '&nbsp; <i class="fa fa-check correct"></i>';
+                correct++;
+            } else {
+                questionResult = '&nbsp;<i class="fa fa-times wrong"></i>';
+            }
+            output = output + '<p>Question ' + (i + 1) + ' ' + questionResult + '</p> ';
+        }
+        output += '<button class="start"><a href="/categories">Play again<i class="fa fa-play-circle-o"></i></a></button><p>Correct answers are ' + data[cateId][levelId][0].correct + ',' + data[cateId][levelId][1].correct + ',' + data[cateId][levelId][2].correct + ',' + data[cateId][levelId][3].correct + ',' + data[cateId][levelId][4].correct + '</p>';
+        output = output + '<p>Hey there , You scored ' + (correct * levelId) + ' points out of ' + (lengthofobject * levelId) + '</p></div> ';
+        document.getElementById("q-container").innerHTML = output;
+
+    } else {
+        //console.log('not answered');
+    }
+}
+
+function moveBack() {
+    curPage--;
+    checkPage(curPage);
+}
+function checkPage() {
+    //add remove disabled buttons 
+    if (curPage == 0) {
+        btnPrevious.classList.add("hide");
+    } else {
+        btnPrevious.classList.remove("hide");
+    }
+    if ((curPage + 1) < (lengthofobject)) {
+        btnNext.classList.remove("hide");
+        submitBtn.classList.add("hide");
+    } else {
+        btnNext.classList.add("hide");
+        submitBtn.classList.remove("hide");
+    }
+
+    var myObj = data[cateId][levelId][curPage];
+    // console.log(myObj);
+    myHeader.innerHTML = ("Level " + levelId);
+    myQuestion.innerHTML = myObj.question;
+
+    newimage.src = myCategoryimages[cateId];
+
+    myAnswerz.innerHTML = "";
+    var addSelClass = '';
+    console.log(myAnswers);
+    // console.log(curPage);
+    // console.log(myAnswers[curPage]);
+    for (var index in myObj.answers) {
+
+        // console.log(parseInt(index) + 1);
+        if (myAnswers[curPage] == (parseInt(index) + 1)) {
+            addSelClass = "selectAnswer";
+        } else {
+            addSelClass = "";
+        }
+        myAnswerz.innerHTML += '<button class="answer ' + addSelClass + ' "> <a data-id="' + (parseInt(index) + 1) + '" class="answer-btn">' + myObj.answers[index] + '</a> </button>';
+
+    }
+
+    for (var i = 0; i < classname.length; i++) {
+        classname[i].addEventListener("click", myAnswer, false);
+        // console.log('myAnswers');
+    }
+
+    //Update progressBar
+    increment = Math.ceil((curPage + 1) / (lengthofobject) * 100);
+    progressBar.style.width = (increment) + '%';
+    progressBar.innerHTML = (curPage + 1) + ' of ' + lengthofobject;
+}
+
